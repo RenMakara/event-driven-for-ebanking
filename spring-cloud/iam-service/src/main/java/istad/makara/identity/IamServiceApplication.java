6d992c44bbb95a580b29e1350211116037545cbc
RenMakara <makara00814@gmail.com>
2026-03-15 05:01:39 +0700

    Add JPA

--- spring-cloud/iam-service/src/main/java/istad/makara/identity/IamServiceApplication.java
+++ spring-cloud/iam-service/src/main/java/istad/makara/identity/IamServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItpIdentityServiceApplication {
+public class IamServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ItpIdentityServiceApplication.class, args);
+        SpringApplication.run(IamServiceApplication.class, args);
     }
 
 }
